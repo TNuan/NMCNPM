fccd38c55713f76c9858e860abcebd28ac9fb0b1
TNuan <minibike090@gmail.com>
2023-02-03 18:35:36 +0700

    Initial views quan ly cuoc hop.

---
+++ src/views/CuocHopManagePanel.java
@@ -0,0 +1,16 @@
+package views;
+// import controllers.CuocHopPanelController;
+import javax.swing.JFrame;
+import views.HoKhauManagerFrame.ChuyenDiNoiKhac;
+import views.HoKhauManagerFrame.TachHoKhau;
+import views.HoKhauManagerFrame.ThemMoiHoKhau;
+
+/**
+ * 
+ * @author Nuan
+ */
+
+public class CuocHopManagePanel extends javax.swing.JPanel {
+    private JFrame parentFrame;
+    // private CuocHopPanelController controller;
+}
---
+++ src/views/CuocHopManagerFrame/ChooseMeetingAttendee.java
@@ -0,0 +1,5 @@
+package views.CuocHopManagerFrame;
+
+public class ChooseMeetingAttendee {
+    
+}
