d6d454e6cfc551e5cf22f704ac3bb5ac1b660d13
TNuan <minibike090@gmail.com>
2023-02-05 10:49:05 +0700

    Update CuocHopPanelController

--- src/controllers/CuocHopPanelController.java
+++ src/controllers/CuocHopPanelController.java
@@ -16,10 +16,105 @@ import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
-// import services.CuocHopService;
-// import utility.TableModelCuocHop;
-// import views.infoViews.InfoJframe;
+import services.CuocHopService;
+import utility.TableModelCuocHop;
+import views.infoViews.InfoJframe;
+
+/**
+ * 
+ * @author Nuan
+ */
 
 public class CuocHopPanelController {
-    
+    private List<CuocHopBean> list;
+    private JTextField searchJtf;
+    private JPanel tableJpn;
+    private final CuocHopService cuocHopService = new CuocHopService();
+    private final TableModelCuocHop tableModelCuocHop = new TableModelCuocHop();
+    private final String COLUNMS[] = { "Mã cuộc họp", "Người tạo", "Tiêu đề" };
+    private JFrame parentJFrame;
+
+    public CuocHopPanelController(JTextField searchJtf, JPanel tableJpn) {
+        this.searchJtf = searchJtf;
+        this.tableJpn = tableJpn;
+        this.list = cuocHopService.getListCuocHop();
+        setData();
+        initAction();
+    }
+
+    private void initAction() {
+        this.searchJtf.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String key = searchJtf.getText().trim();
+                if (key.isEmpty()) {
+                    list = cuocHopService.getListHoKhau();
+                } else {
+                    list = cuocHopService.search(key);
+                }
+                setData();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String key = searchJtf.getText().trim();
+                if (key.isEmpty()) {
+                    list = cuocHopService.getListHoKhau();
+                } else {
+                    list = cuocHopService.search(key);
+                }
+                setData();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                String key = searchJtf.getText().trim();
+                if (key.isEmpty()) {
+                    list = cuocHopService.getListHoKhau();
+                } else {
+                    list = cuocHopService.search(key);
+                }
+                setData();
+            }
+        });
+    }
+
+    private void setData() {
+        DefaultTableModel model = tableModelCuocHop.setTableHoKhau(list, COLUNMS);
+
+        JTable table = new JTable(model) {
+            @Override
+            public boolean editCellAt(int row, int column, EventObject e) {
+                return false;
+            }
+
+        };
+        table.getTableHeader().setFont(new Font("Arial", Font.BOLD, 14));
+        table.getTableHeader().setPreferredSize(new Dimension(100, 50));
+        table.setRowHeight(50);
+        table.validate();
+        table.repaint();
+        table.setFont(new Font("Arial", Font.PLAIN, 14));
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() > 1) {
+                    CuocHopBean temp = list.get(table.getSelectedRow());
+                    InfoJframe infoJframe = new InfoJframe(temp.toString(), parentJFrame);
+                    infoJframe.setLocationRelativeTo(null);
+                    infoJframe.setVisible(true);
+                }
+            }
+
+        });
+
+        JScrollPane scroll = new JScrollPane();
+        scroll.getViewport().add(table);
+        tableJpn.removeAll();
+        tableJpn.setLayout(new BorderLayout());
+        tableJpn.add(scroll);
+        tableJpn.validate();
+        tableJpn.repaint();
+    }
+
 }
