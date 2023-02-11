9676268642ffef27761492136c45c491c7d1044c
TNuan <minibike090@gmail.com>
2023-02-11 21:52:55 +0700

    Update view Delete Cuoc Hop.

--- src/views/CuocHopManagerFrame/DeleteCuocHop.java
+++ src/views/CuocHopManagerFrame/DeleteCuocHop.java
@@ -31,7 +31,7 @@ public class DeleteCuocHop extends javax.swing.JFrame {
         this.parentJFrame = parentJFrame;
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.parentJFrame.setEnabled(false);
-        controller = new XoaCuocHopController(cuocHopBean, maCuocHopJtf, tableCuocHopJpn);
+        controller = new XoaCuocHopController(cuocHopBean, maCuocHopJtf, tableJpn);
 
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -50,13 +50,6 @@ public class DeleteCuocHop extends javax.swing.JFrame {
         }
     }
 
-    private void checkPassword() {
-        String tempPassword = this.passwordJtf.getText().trim();
-        if (tempPassword.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Vui lòng nhập mật khẩu", "Warning!!", JOptionPane.WARNING_MESSAGE);
-            return;
-        }
-    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -65,36 +58,25 @@ public class DeleteCuocHop extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated
-    // Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        passwordJtf = new javax.swing.JTextField();
         xacNhanBtn = new javax.swing.JButton();
         huyBtn = new javax.swing.JButton();
-        checkPasswordBtn = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         maCuocHopJtf = new javax.swing.JTextField();
-        checkMaCuocHopBtn = new javax.swing.JButton();
-        jLabel8 = new javax.swing.JLabel();
-        tableCuocHopJpn = new javax.swing.JPanel();
+        tableJpn = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jLabel1.setText("Password");
-
-        passwordJtf.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-
         xacNhanBtn.setText("Xác nhận");
-
         xacNhanBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                xacNhanBtnActionPerformed(evt);
-            }
-        });
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                        xacNhanBtnActionPerformed(evt);
+                }
+            });
+        
 
         huyBtn.setText("Hủy");
         huyBtn.addActionListener(new java.awt.event.ActionListener() {
@@ -103,148 +85,81 @@ public class DeleteCuocHop extends javax.swing.JFrame {
             }
         });
 
-        checkPasswordBtn.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        checkPasswordBtn.setText("Check");
-        checkPasswordBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                checkPasswordBtnActionPerformed(evt);
-            }
-        });
-
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/checked.png"))); // NOI18N
-        jLabel3.setEnabled(false);
-
         jLabel7.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLabel7.setText("Mã cuộc họp");
 
         maCuocHopJtf.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-
-        checkMaCuocHopBtn.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        checkMaCuocHopBtn.setText("Check");
-        checkMaCuocHopBtn.addActionListener(new java.awt.event.ActionListener() {
+        maCuocHopJtf.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                checkMaCuocHopBtnActionPerformed(evt);
+                maCuocHopJtfActionPerformed(evt);
             }
         });
 
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/checked.png"))); // NOI18N
-        jLabel8.setEnabled(false);
+        tableJpn.setBackground(new java.awt.Color(204, 204, 204));
 
-        tableCuocHopJpn.setBackground(new java.awt.Color(204, 204, 204));
-
-        javax.swing.GroupLayout tableCuocHopJpnLayout = new javax.swing.GroupLayout(tableCuocHopJpn);
-        tableCuocHopJpn.setLayout(tableCuocHopJpnLayout);
-        tableCuocHopJpnLayout.setHorizontalGroup(
-                tableCuocHopJpnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 0, Short.MAX_VALUE));
-        tableCuocHopJpnLayout.setVerticalGroup(
-                tableCuocHopJpnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 330, Short.MAX_VALUE));
+        javax.swing.GroupLayout tableJpnLayout = new javax.swing.GroupLayout(tableJpn);
+        tableJpn.setLayout(tableJpnLayout);
+        tableJpnLayout.setHorizontalGroup(
+            tableJpnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        tableJpnLayout.setVerticalGroup(
+            tableJpnLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 368, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
-                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addGap(0, 0, Short.MAX_VALUE)
-                                                .addComponent(huyBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 80,
-                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addGap(18, 18, 18)
-                                                .addComponent(xacNhanBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 80,
-                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addContainerGap())
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addGroup(jPanel1Layout
-                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                                .addComponent(jLabel1)
-                                                                .addPreferredGap(
-                                                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-                                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                                        Short.MAX_VALUE)
-                                                                .addComponent(passwordJtf,
-                                                                        javax.swing.GroupLayout.PREFERRED_SIZE, 274,
-                                                                        javax.swing.GroupLayout.PREFERRED_SIZE))
-                                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                                .addComponent(jLabel7)
-                                                                .addPreferredGap(
-                                                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-                                                                        61, Short.MAX_VALUE)
-                                                                .addComponent(maCuocHopJtf,
-                                                                        javax.swing.GroupLayout.PREFERRED_SIZE, 274,
-                                                                        javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                .addGroup(jPanel1Layout
-                                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                                .addComponent(checkPasswordBtn)
-                                                                .addPreferredGap(
-                                                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                                .addComponent(jLabel3))
-                                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                                .addComponent(checkMaCuocHopBtn)
-                                                                .addPreferredGap(
-                                                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                                .addComponent(jLabel8)))
-                                                .addGap(14, 14, 14))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addComponent(tableCuocHopJpn, javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                .addContainerGap()))));
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(tableJpn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel7)
+                        .addGap(59, 59, 59)
+                        .addComponent(maCuocHopJtf))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 505, Short.MAX_VALUE)
+                        .addComponent(huyBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(xacNhanBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
         jPanel1Layout.setVerticalGroup(
-                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 30,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(passwordJtf, javax.swing.GroupLayout.PREFERRED_SIZE, 30,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(checkPasswordBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 30,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jLabel3))
-                                .addGap(18, 18, 18)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 30,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(maCuocHopJtf, javax.swing.GroupLayout.PREFERRED_SIZE, 30,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(checkMaCuocHopBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 30,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jLabel8))
-                                .addGap(18, 18, 18)
-                                .addComponent(tableCuocHopJpn, javax.swing.GroupLayout.DEFAULT_SIZE,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(18, 18, 18)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(xacNhanBtn)
-                                        .addComponent(huyBtn))
-                                .addContainerGap()));
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(maCuocHopJtf, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(19, 19, 19)
+                .addComponent(tableJpn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(xacNhanBtn)
+                    .addComponent(huyBtn))
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
-                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
         layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
-                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void checkMaCuocHopBtnActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_checkMaCuocHopBtnActionPerformed
+    private void maCuocHopJtfActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_checkMaCuocHopBtnActionPerformed
         // TODO add your handling code here:
-    }// GEN-LAST:event_checkMaCuocHopBtnActionPerformed
-
-    private void checkPasswordBtnActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_checkPasswordBtnActionPerformed
-        // TODO add your handling code here:
-    }// GEN-LAST:event_checkPasswordBtnActionPerformed
+    }// GEN-LAST:event_maCuocHopJtfActionPerformed
 
     private void huyBtnActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_huyBtnActionPerformed
         close();
@@ -264,17 +179,11 @@ public class DeleteCuocHop extends javax.swing.JFrame {
     }// GEN-LAST:event_huyBtnActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton checkPasswordBtn;
     private javax.swing.JButton xacNhanBtn;
     private javax.swing.JButton huyBtn;
-    private javax.swing.JButton checkMaCuocHopBtn;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField passwordJtf;
     private javax.swing.JTextField maCuocHopJtf;
-    private javax.swing.JPanel tableCuocHopJpn;
+    private javax.swing.JPanel tableJpn;
     // End of variables declaration//GEN-END:variables
 }
